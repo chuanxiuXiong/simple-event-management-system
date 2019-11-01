8eb218ecb0144c267c9853f43d2ab550eddc85ab
Chuanxiu Xiong <chuanxiuxiong@outlook.com>
2019-11-01 13:30:23 -0700

    server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
+const loaders = require('./loaders');
+const config = require('./config');
 const express = require('express');
+const { Container } = require('typedi');
+
+async function startServer() {
+  const app = express();
+  await loaders.default({ expressApp: app });
+  app.listen(config.port, err => {
+    if (err) {
+      process.exit(1);
+      return;
+    }
+    console.log(`Server listening on port: ${config.port}`);
+  });
+}
+
+startServer();
